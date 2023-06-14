3e9a0e3c430683c23e8bde648cc531799cef8a25
moskiup <moskiup@gmail.com>
2023-06-14 14:57:53 -0600

    update heroslider

--- src/components/heroslider/Heroslider.jsx
+++ src/components/heroslider/Heroslider.jsx
@@ -1,25 +1,25 @@
-import tmdApi, { movieType } from "./../../api/tmdbApi.js";
-import { useEffect, useState } from "react";
+// import tmdApi, { movieType } from "./../../api/tmdbApi.js";
+// import { useEffect, useState } from "react";
 import "./hero-slider.scss";
 
 
 export function HeroSlider () {
   
 
-  useEffect(() => {
-    const getMovies = async () => {
-      try{
-        const response = await tmdApi.getMovieList(movieType.upcoming );
-        setMovies(response.results.slice(1,4));
-        console.log(response);
+  // useEffect(() => {
+  //   const getMovies = async () => {
+  //     try{
+  //       const response = await tmdApi.getMovieList(movieType.upcoming );
+  //       setMovies(response.results.slice(1,4));
+  //       console.log(response);
 
-    }catch {
-      console.log('error')
-    }
-    }
-    getMovies();
+  //   }catch {
+  //     console.log('error')
+  //   }
+  //   }
+  //   getMovies();
     
-  }, []);
+  // }, []);
 
   return (
     <div className="hero-slider">
