a5a774a15edfc053c1b3e9b05fa144fe09bd2ab1
moskiup <moskiup@gmail.com>
2023-06-18 11:31:09 -0600

    modificacion skeleton movie card

--- src/components/moviecard/MovieCard.jsx
+++ src/components/moviecard/MovieCard.jsx
@@ -1,18 +1,25 @@
+import { useState } from 'react';
 import './MovieCard.scss';
 
 export function MovieCard(props) {
   // const { title , overview , popularity , vote_average , }
 
+  const [isLoading, setLoading] = useState(true);
   const { movie } = props;
   const imagen_url = 'https://image.tmdb.org/t/p/w300/' + movie.poster_path;
 
+  function handlerLoad() {
+    setLoading(false);
+  }
+
   return (
     <>
-      <div className="movie-card">
+      <div className={isLoading ? 'movie-card skeleton' : 'movie-card'}>
         {/* <p>{movie.overview}</p> */}
-        <p>{}</p>
-        {/* <p>{movie.vote_average}</p> */}
-        <img src={imagen_url} />
+        <span>
+          <img src={imagen_url} onLoad={handlerLoad} />
+        </span>
+
         <h1>{`${movie.title || movie.original_name}`}</h1>
       </div>
     </>
