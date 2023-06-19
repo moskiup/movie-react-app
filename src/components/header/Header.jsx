9edc1b0c7a204c294e7b3ef6bdc1ca7d1d7f1d54
moskiup <moskiup@gmail.com>
2023-06-19 00:45:29 -0600

    correccion de header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,21 +1,22 @@
-import "./header.scss";
-import { NavLink  } from "react-router-dom";
-import { useEffect, useState } from "react";
+import './header.scss';
+import { NavLink, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import logo from './../../../public/logo.png';
 
 export function Header() {
   const MenuList = [
-    { name: "Home", path: "/" },
-    { name: "Movies", path: "/movies" },
-    { name: "TV Series", path: "/series" },
+    { name: 'Home', path: '/' },
+    { name: 'Movies', path: '/movies' },
+    { name: 'TV Series', path: '/series' },
   ];
 
-  const [isSticky , setIsSticky] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const navbar = document.getElementById('navbar');
       const navbarOffsetTop = navbar.offsetTop;
-      if (scrollPosition-100 >= 0) {
+      if (scrollPosition - 100 >= 0) {
         setIsSticky(true);
       } else {
         setIsSticky(false);
@@ -29,20 +30,25 @@ export function Header() {
     };
   }, []);
 
-
   return (
     <header id="header" className={`navbar ${isSticky ? 'sticky' : ''}`}>
-      <nav id="navbar" >
-        <h1>🎥 PELICULAS</h1>
+      <nav id="navbar">
+        <Link to="/">
+          <div className="logo-container">
+            <img src={logo} className="logo" />
+            <h1>MOVIES</h1>
+          </div>
+        </Link>
         <ul>
           {MenuList.map((x) => {
             return (
-              <NavLink  key={x.name} className={({ isActive }) =>  isActive ? "active" : ""} to={x.path}>
-              <li key={x.name}>
-              
-                  {x.name}
-              </li>
-                </NavLink >
+              <NavLink
+                key={x.name}
+                className={({ isActive }) => (isActive ? 'active' : '')}
+                to={x.path}
+              >
+                <li key={x.name}>{x.name}</li>
+              </NavLink>
             );
           })}
         </ul>
