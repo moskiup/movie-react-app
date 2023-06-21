6592973875cb9bcd8687b1684e095e4645e59163
moskiup <moskiup@gmail.com>
2023-06-20 20:15:22 -0600

    separating the login with the hook

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,35 +1,7 @@
 import { HeroSlider } from '@components/heroslider/HeroSlider.jsx';
 import { ListSlide } from '@components/list-slide/ListSlide.jsx';
-import tmdApi, { tvType, movieType } from '../api/tmdbApi.js';
-
-import { useState, useEffect } from 'react';
 import { Loader } from '@components/loader/Loader.jsx';
-
-function useLandingFetch() {
-  const [topMovies, setTopMovies] = useState(null);
-  const [topSeries, setTopSeries] = useState(null);
-  const [trendSeries, setTrendSeries] = useState(null);
-  const [trendMovies, setTrendMovies] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const data_top_mov = await tmdApi.getMovieList(movieType.top_rated);
-      const data_top_tv = await tmdApi.getTvList(tvType.top_rated);
-      const data_trend_tv = await tmdApi.getTvList(tvType.on_the_air);
-      const data_trend_movies = await tmdApi.getMovieList(movieType.popular);
-
-      setTopMovies(data_top_mov.results);
-      setTopSeries(data_top_tv.results);
-      setTrendSeries(data_trend_tv.results);
-      setTrendMovies(data_trend_movies.results);
-      setIsLoading(false);
-    };
-    getMovies();
-  }, []);
-
-  return { isLoading, trendMovies, trendSeries, topSeries, topMovies };
-}
+import { useLandingFetch } from '@/hooks/useLandingFetch';
 
 export function LandingPage() {
   const { isLoading, trendMovies, trendSeries, topSeries, topMovies } = useLandingFetch();
